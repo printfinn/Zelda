41ee89dc9af687ce75887d6051c1c07a349ce875
finn <git@printf.win>
2020-01-03 20:50:08 +0800

    modify footer, add redirect after sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/goodbye'
 end
