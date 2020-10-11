82f52fd51d6b4ad614119c9e73f28a42437b13b6
printfinn <git@printf.win>
2020-10-11 19:47:18 +0800

    Autoformat with rubocop -x

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,105 +1,102 @@
 class DevicesController < ApplicationController
+  before_action :authenticate_user!
 
-	before_action :authenticate_user!
+  def index
+    @devices = Device.all.order(device_order: :asc)
+    @photo_name = ""
 
-	def index
-		@devices = Device.all.order(device_order: :asc)
-		@photo_name = ""
+    # This session is set in trigger_capture_command method.
+    if session[:photo_name] && !session[:photo_name].empty?
+      @photo_name = session[:photo_name]
+    end
+    session[:photo_name] = ""
+  end
 
-		# This session is set in trigger_capture_command method.
-		if session[:photo_name] && !session[:photo_name].empty?
-			@photo_name = session[:photo_name]
-		end
-		session[:photo_name] = ""
-	end
+  def new
+    @device = Device.new
+  end
 
+  def create
+    @device = Device.new(device_params)
 
-	def new
-		@device = Device.new
-	end
+    if @device.save
+      flash[:info] = "You have successfully created a new device: #{@device.device_name}"
+      redirect_to @device
+    else
+      render 'new'
+    end
+  end
 
-	def create
-		@device = Device.new(device_params)
+  def show
+    @device = Device.find(params[:id])
+  end
 
-		if @device.save
-			flash[:info] = "You have successfully created a new device: #{@device.device_name}"
-			redirect_to @device
-		else
-			render 'new'
-		end
-	end
+  def edit
+    @device = Device.find(params[:id])
+  end
 
-	def show
-		@device = Device.find(params[:id])
-	end
-
-	def edit
-		@device = Device.find(params[:id])
-	end
-
-	def update
+  def update
     @device = Device.find(params[:id])
 
     if @device.update(device_params)
-    	flash[:info] = "You have successfully updated #{@device.device_name}"
+      flash[:info] = "You have successfully updated #{@device.device_name}"
       redirect_to @device
     else
       render 'edit'
     end
   end
 
-	def destroy
-		@device = Device.find(params[:id])
-		if @device.destroy
-			flash[:info] = "You have successfully removed #{@device.device_name}."
-			redirect_to devices_path
-		else
-			flash[:alarm] = "Something is wrong, please contact admin or create a new issue on Github."
-		end
-	end
+  def destroy
+    @device = Device.find(params[:id])
+    if @device.destroy
+      flash[:info] = "You have successfully removed #{@device.device_name}."
+      redirect_to devices_path
+    else
+      flash[:alarm] = "Something is wrong, please contact admin or create a new issue on Github."
+    end
+  end
 
+  def trigger_on_command
+    trigger
+  end
 
-	def trigger_on_command
-		trigger
-	end
+  def trigger_off_command
+    trigger
+  end
 
-	def trigger_off_command
-		trigger
-	end
+  def trigger_capture_command
+    @device = Device.find(params[:id])
+    cmd = @device.on_command
+    system(cmd)
+    session[:photo_name] = "Photo from #{@device.device_name}"
+    redirect_to devices_path # camera_photo_device_path
+  end
 
-	def trigger_capture_command
-		@device = Device.find(params[:id])
-		cmd = @device.on_command
-		system(cmd)
-		session[:photo_name] = "Photo from #{@device.device_name}"
-		redirect_to devices_path#camera_photo_device_path
-	end
+  private
 
-	private
-		def device_params
-	    params.require(:device).permit(:device_name, :device_type, :device_location, :on_command, :off_command, :device_order)
-	  end
+  def device_params
+    params.require(:device).permit(:device_name, :device_type, :device_location, :on_command, :off_command, :device_order)
+  end
 
-	  def trigger
-	  	@device = Device.find(params[:id])
-	  	instruction = params[:action].split('_')[1]
-	  	# action can be: trigger_on_command / trigger_off_command
-			cmd = case instruction
-						when "on"
-							@device.on_command
-						when "off"
-							@device.off_command
-						else
-							""
-						end
+  def trigger
+    @device = Device.find(params[:id])
+    instruction = params[:action].split('_')[1]
+    # action can be: trigger_on_command / trigger_off_command
+    cmd = case instruction
+          when "on"
+            @device.on_command
+          when "off"
+            @device.off_command
+          else
+            ""
+          end
 
-			job1 = fork do
-			  system(cmd)
-			end
-			Process.detach(job1)
-			message = "Turning #{@device.device_name} #{instruction.upcase}"
-			flash[:primary] = message
-			redirect_to devices_path
-		end
-
+    job1 = fork do
+      system(cmd)
+    end
+    Process.detach(job1)
+    message = "Turning #{@device.device_name} #{instruction.upcase}"
+    flash[:primary] = message
+    redirect_to devices_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :devices do
-  	member do
-	  	post 'on', to: 'devices#trigger_on_command'
-	  	post 'off', to: 'devices#trigger_off_command'
-	  	post 'capture', to: 'devices#trigger_capture_command'
-	  end
-	end
+    member do
+      post 'on', to: 'devices#trigger_on_command'
+      post 'off', to: 'devices#trigger_off_command'
+      post 'capture', to: 'devices#trigger_capture_command'
+    end
+  end
 
   get 'static_pages/home'
   get 'static_pages/documentation'
   get 'static_pages/goodbye'
-
-
-
 end
