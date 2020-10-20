55e48e1b96f5b7ad641f8613dd26ea27cc39d8ef
printfinn <git@printf.win>
2020-10-20 08:08:24 +0800

    Add a button to switch between languages (i18n). Remove some :get code from link_to.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@
 class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
   # before_action :authenticate_user!
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   private
     # Its important that the location is NOT stored if:
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,11 @@ module HiApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Permitted locales available for the application
+    config.i18n.available_locales = [:en, :zh]
+
+    # Set default locale to something other than :en
     config.i18n.default_locale = :zh
   end
 end
