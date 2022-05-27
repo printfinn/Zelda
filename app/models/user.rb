32d3d60119166f41e6d86fa92b6630b5da189fd0
printfinn <git@printf.win>
2022-05-27 14:35:48 +0800

    Remove email related devise features.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable
+         :rememberable, :validatable
+         #:recoverable, :confirmable, :lockable
 end
