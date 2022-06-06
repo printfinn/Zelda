fabec1f334f1771b34549895567788400df54262
printfinn <git@printf.win>
2022-06-06 20:24:41 +0800

    Skip verify_authenticity_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   before_action :store_user_location!, if: :storable_location?
   # before_action :authenticate_user!
   around_action :switch_locale
